46f43730e3b0a807e836c4e632c5f2521ff8114e
KondratevProkhor <prokhor-2000@mail.ru>
2024-05-23 10:23:37 +0300

    - стилизация рабочих нод

--- src/worknodes/js/worknodes_handler.js
+++ src/worknodes/js/worknodes_handler.js
@@ -26,13 +26,79 @@ class WorkNode {
                 </div>
             </div>
             <div class="wn__content">
-                <div class="wn-cont__item">
-                    <span class="span-label">Хост:</span><span>${this.host}</span>
-                    <span class="span-label">Порт:</span><span>${this.port}</span>
+                <div class="wn-cont__connection">
+                    <h5>Подключение</h5>
+                    <div class="wn-cont__connection__item">
+                        <div>
+                            <span class="label">Хост:</span>
+                            <span class="info">${this.host}</span>
+                        </div>
+                        <div>
+                            <span class="info">Порт:</span>
+                            <span class="info">${this.port}</span>
+                        </div>
+                    </div>
                 </div>
-                <div class="wn-cont__item">
-                    <span class="span-label">Статус:</span><span>Не активен</span>
-                    <button class="wn__button"><svg><use xlink:href="${BIcon}#arrow-repeat"></use></svg></button>
+                <div class="wn-cont__status">
+                    <h5>Статус</h5>
+                    <div class="wn-cont__status__item">
+                        <span>Активен</span>
+                        <svg><use xlink:href="${BIcon}#check-circle"></use></svg>
+                    </div>
+                </div>
+                <div class="wn-cont__processing">
+                    <h5>Обработка</h5>
+                    <div class="wn-cont__processing__item">
+                        <div class="wn-cont__processing-info">
+                            <div>
+                                <span class="label">В работе:</span>
+                                <span class="info">1</span>
+                            </div>
+                            <div>
+                                <span class="label">В очереди:</span>
+                                <span class="info">10</span>
+                            </div>
+                            <div>
+                                <span class="label">Успешных:</span>
+                                <span class="info">100</span>
+                            </div>
+                            <div>
+                                <span class="label">С ошибками:</span>
+                                <span class="info">5</span>
+                            </div>
+                        </div>
+                        <button class="wn__button wide">Подробнее</button>
+                    </div>
+                </div>
+                <div class="wn-cont__specs">
+                    <h5>Характеристики</h5>
+                    <div class="wn-cont__specs__item">
+                        <div>
+                            <span class="label">CPU:</span>
+                            <span class="info">Intel Core i7</span>
+                        </div>
+                        <div>
+                            <span class="label">RAM:</span>
+                            <span class="info">16 ГБ</span>
+                        </div>
+                        <div>
+                            <span class="label">GPU:</span>
+                            <span class="info">NVIDIA GeForce GTX 1050</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="wn-cont__diskspace">
+                    <h5>Дисковое пространство</h5>
+                    <div class="wn-cont__diskspace__item">
+                        <div>
+                            <span class="label">Всего:</span>
+                            <span class="info">1 ТБ</span>
+                        </div>
+                        <div>
+                            <span class="label">Использовано:</span>
+                            <span class="info">500 ГБ</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         `;
